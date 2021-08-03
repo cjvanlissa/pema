56d2cb310e3d5fd9d7633799c426b66bdc6611a9
Eli-Boaz Clapper <e.clapper@uu.nl>
2021-08-03 08:24:56 +0200

    First part of the brma_sim() function works. the 'fits' and 'selected' parts remain
    I also put the functions for simulations in a seperate folder for organizational purposes

--- simulatie_2021/brma_parallel_models.R
+++ simulatie_2021/brma_parallel_models.R
@@ -16,6 +16,7 @@ clusterEvalQ(cl, library(rstan))
 clusterEvalQ(cl, library(brms))
 clusterEvalQ(cl, library(sn))
 
+
 #create list of simulated datasets to test parallel functions
 simulated_data <- list(pema::simulate_smd(), pema::simulate_smd())
 
@@ -46,12 +47,85 @@ hs_models <- parLapply(cl, simulated_data, function(data) {
 #stop clusters
 stopCluster(cl)
 
+
+#------work from here
+brma_for_sim <- function(...){
+  args <- as.list(match.call()[-1])
+  res <- try(do.call(brma, args), silent = TRUE)
+}
+
+clusterExport(cl, "brma_for_sim")
+
 #upper part of brma for simulation function. Note the extra 'method' argument to specify either horseshoe or Lasso
+#this part works
 brma_sim <- function(cl, simulated_data, file_stem, method, ...){
   brma_models <- parLapply(cl, simulated_data, function(data) {
-    brma(yi ~ .,
-         data = data$training,
-         method = method,
-         ...)
+    args <- list(formula = force(as.formula(paste0("yi~", paste0(names(data$training)[-c(1:2)], collapse = "+")))),
+                            vi = data$training$vi,
+                            study = 1:nrow(data$training),
+                            data = data$training,
+                            method = 'lasso')
+    do.call(brma_for_sim, args)
   })
+}
+
+
+rma_sim <- function(cl, simulated_data, file_stem, ...){
+  rma_models <- parLapply(cl, simulated_data, function(data) {
+    args <- list(yi = data$training$yi,
+                 vi = data$training$vi,
+                 mods = as.matrix(data$training[,-c(1:2)]))
+
+    do.call(rma_for_sim, args)
+  })
+
+  rma_fits <- t(
+    clusterMap(
+      cl,
+      fun = function(models, data) {
+        if(is.na(models)){
+          return(rep(NA, 7))
+        }
+        c(
+          model_accuracy(
+            fit = models,
+            newdata = as.matrix(data$training[, -c(1:2)]),
+            observed = data[[1]]$yi
+          ),
+          model_accuracy(
+            fit = models,
+            newdata = as.matrix(data$testing[, -1]),
+            observed = data$testing$yi,
+            ymean = mean(data$training$yi)
+          ),
+          tau2 = models$tau2
+        )
+      },
+      models = rma_models,
+      data = simulated_data,
+      SIMPLIFY = TRUE,
+      USE.NAMES = FALSE
+    )
+  )
+
+  rma_selected <-
+    t(parSapply(
+      cl = cl,
+      rma_models,
+      FUN = function(models) {
+        models[["beta"]][-1,1]
+      }
+    ))
+
+  file_name <-
+    paste0(paste(c(file_stem, "fits", chunk), collapse = "_"), ".RData")
+  saveRDS(rma_fits, file = file_name, compress = FALSE)
+
+  file_name <-
+    paste0(paste(c(file_stem, "selected", chunk), collapse = "_"), ".RData")
+  saveRDS(rma_selected,
+          file = file_name,
+          compress = FALSE)
+
+  rm(rma_models, rma_fits, rma_selected)
 }
