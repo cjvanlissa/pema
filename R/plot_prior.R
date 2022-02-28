806ebd4ee5a1bfe1ded9f1d8a6fb73801b81b6b8
cjvanlissa <c.j.vanlissa@uu.nl>
2022-02-28 11:42:53 +0100

    update 2022-02-28 11:42:53

--- R/plot_prior.R
+++ R/plot_prior.R
@@ -1,9 +1,25 @@
+#' @title Plot Prior Distribution
+#' @description Plots the kernal density function of samples from a prior
+#' distribution with parameters defined in `prior`.
+#' @param method Character string, indicating which prior to sample from.
+#' Default: first element of `c("hs", "lasso")`.
+#' @param prior Numeric vector, specifying the prior to use. See [pema::brma]
+#' for more details.
+#' @param iter A positive integer specifying the number of iterations to sample.
+#' Default: 1000
+#' @return NULL, function is called for its side-effect of plotting to the
+#' graphics device.
+#' @examples
+#' plot_prior("lasso", iter = 10)
+#' @rdname plot_prior
+#' @importFrom rstan sampling
+#' @export
 plot_prior <- function(method = c("hs", "lasso"),
                        prior = switch(method,
                                       "lasso" = c(df = 1, scale = 1),
                                       "hs" = c(df = 1, df_global = 1, df_slab = 4, scale_global = 1, scale_slab = 1, par_ratio = NULL)),
                        iter = 1000){
-  smpls <- suppressWarnings(sampling(object = pema:::stanmodels[[c("lasso_prior", "hs_prior")[(method[1] == "hs")+1]]], data = as.list(prior), chains = 1, iter = 10000, warmup = 0, show_messages = FALSE, verbose = FALSE, refresh = 0))
+  smpls <- suppressWarnings(sampling(object = stanmodels[[c("lasso_prior", "hs_prior")[(method[1] == "hs")+1]]], data = as.list(prior), chains = 1, iter = iter, warmup = 0, show_messages = FALSE, verbose = FALSE, refresh = 0))
   plot(density(smpls@sim$samples[[1]]$b), main = c("Lasso prior", "Horseshoe prior")[(method == "hs")+1],
        xlab = paste0("Samples: ", iter, ", ", paste0(names(prior), " = ", prior, collapse = ", ")),
        xlim = c(-5, 5))
